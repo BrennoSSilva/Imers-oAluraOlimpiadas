09c4d07dc610312ffb02f5863ad595cfa932deea
Brenno S Silva <brennosoares2@gmail.com>
2024-09-09 20:51:00 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Com um talento inegável e uma determinação ímpar, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e força.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        linkYoutube : "https://www.youtube.com/watch?v=U6QlKjfTSpc",
+        tags : "ouro ginastica "
+    
+    },
+    
+    {
+        titulo : "Rayssa Leal",
+        descricao : " Rayssa Leal é uma jovem skatista brasileira que conquistou o mundo com seu talento e carisma. Conhecida como Fadinha, ela se tornou a mais jovem medalhista olímpica brasileira, ao conquistar a prata nos Jogos Olímpicos de Tóquio 2020. ",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        linkYoutube : "https://www.youtube.com/watch?v=Kc4u0H27vLE",
+        tags: "skate fada sk8"
+    
+    },
+    
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é um nome que brilha no cenário do judô mundial. Com uma força e determinação impressionantes, a judoca brasileira conquistou o coração dos fãs e se tornou um símbolo de superação e sucesso",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        linkYoutube : "https://www.youtube.com/watch?v=LOJrMq-IwgA",
+        tags : "judô judo judoka "
+
+    }
+];
